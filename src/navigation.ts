8a458c6acc507dd6dd059de4aaf91ad5ee937a69
Aleksey Simonenko <aleksey@MacBook-Pro-Aleksey.local>
2025-12-09 11:57:45 +0400

    Fix: заменил несуществующую иконку brand-viber на phone

--- src/navigation.ts
+++ src/navigation.ts
@@ -63,7 +63,7 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: '#' },
     { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: '#' },
-    { ariaLabel: 'Viber', icon: 'tabler:brand-viber', href: '#' },
+    { ariaLabel: 'Viber', icon: 'tabler:phone', href: '#' },
   ],
   footNote: `
     \u0420\u043e\u0441\u0442\u043e\u0432 \u0414\u0435\u0437\u2014\u0441\u0430\u043d\u0438\u0442\u0430\u0440\u043d\u0430\u044f \u0437\u0430\u0449\u0438\u0442\u0430 800+ \u043f\u0440\u0435\u0434\u043f\u0440\u0438\u044f\u0442\u0438\u0439 \u0420\u043e\u0441\u0442\u043e\u0432\u0430-\u043d\u0430-\u0414\u043e\u043d\u0443. \u0420\u0430\u0431\u043e\u0442\u0430\u0435\u043c \u043d\u043e\u0447\u044c\u044e, \u0447\u0442\u043e\u0431\u044b \u0432\u0430\u0448 \u0431\u0438\u0437\u043d\u0435\u0441 \u0431\u044b\u043b \u0447\u0438\u0441\u0442\u044b\u043c \u0434\u043d\u0451\u043c.
